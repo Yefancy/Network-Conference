4e7ef2a79c493da325f889101ddef00df8ab0a77
Yefancy <471283649@qq.com>
2018-03-19 21:20:00 +0800

    自定义结果类和实现接口

---
+++ src/NCLib/example.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCLib
+{
+    /// <summary>
+    /// 自定义结果类
+    /// </summary>
+    public class exResult : Result
+    {
+        /// <summary>
+        /// 添加新结果类型（举例）
+        /// </summary>
+        public int Num;
+        public exResult(baseResult baseResult, int num, string info = null) : base(baseResult, info)
+        {
+            this.Num = num;
+        }
+    }
+
+    class example : IServerCallDatabase
+    {
+        static void main()
+        {
+            example ex = new example();
+            Result result = ex.AddFriend("", "");
+            Console.WriteLine(result.BaseResult+result.Info);//能识别基本结果类的结果
+            Console.WriteLine((result as exResult).Num);//在知道结果类型下 查看强制转换后的特有结果信息
+
+            //输出结果:
+            //1失败啦
+            //23
+        }
+
+        /// <summary>
+        /// 实现该接口返回自定义结果
+        /// </summary>
+        public Result AddFriend(string userId, string friendId)
+        {
+            exResult result = new exResult(baseResult.Faild, 23, "失败啦");
+            return result;
+        }
+
+        #region 未实现接口
+        public Result AddUser(UserInfo info, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CloseDatabase()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Result ConnectDatabase(string user, string password, string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteFriend(string userId, string friendId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletUser(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Result ExecuteStructuredQueryLanguage(string sql)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
